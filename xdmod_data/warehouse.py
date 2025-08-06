d4c96dbb7a2a33c0f05b24e6f656a515e737e5fa
Aaron Weeden <aaronwee@buffalo.edu>
2025-08-06 11:17:07 -0400

    Warn instead of throwing exception if raw data stream is closed unexpectedly.

--- xdmod_data/warehouse.py
+++ xdmod_data/warehouse.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np
 import os
 import pandas as pd
@@ -49,6 +50,8 @@ class DataWarehouse:
             if xdmod_host is None:
                 raise TypeError(_error_messages.MISSING_XDMOD_HOST) from None
         self.__http_requester = _HttpRequester(xdmod_host)
+        self.__logger = self.__init_logger()
+        self.__http_requester = _HttpRequester(xdmod_host, self.__logger)
         self.__descriptors = _Descriptors(self.__http_requester)
 
     def __enter__(self):
@@ -453,6 +456,15 @@ class DataWarehouse:
             return None
         d = self.__descriptors._get_aggregate()
         return d[realm]["dimensions"][dimension_id]["label"]
+
+    def __init_logger(self):
+        logger = logging.getLogger('xdmod_data_warehouse')
+        logger.setLevel(logging.WARNING)
+        formatter = logging.Formatter('Warning: %(message)s')
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        return logger
 
     def __get_data_frame(self, data, column_data, index=None):
         result = pd.DataFrame(
