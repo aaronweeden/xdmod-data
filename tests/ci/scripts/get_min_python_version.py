2c2f236c2679e4b1d687feadb4d71514686f9025
Aaron Weeden <aaronwee@buffalo.edu>
2026-08-31 21:48:20 -0400

    Update.

--- tests/ci/scripts/get_min_python_version.py
+++
@@ -1,7 +0,0 @@
-# Read the minimum supported Python version from setup.cfg
-import configparser
-from pathlib import Path
-project_dir = Path(__file__).resolve().parents[3]
-config = configparser.ConfigParser()
-config.read(project_dir / 'setup.cfg')
-print(config['options']['python_requires'].removeprefix('>='))
